d58be97ee5a1176d992e2cdb02cb1c4af1a982d2
Egor <alexandrovich2572@gmail.com>
2019-11-25 15:22:40 +0300

    refactor: apply eslint

--- src/script.js
+++ src/script.js
@@ -1,91 +1,64 @@
-let canvas = document.getElementById('my-canvas');
-let ctx = canvas.getContext('2d');
-let picture = new Image();
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+const picture = new Image();
 let cellSize = 0;
 
-let myJson32 = [];
-let myJson =[];
-function fillCanvas2(arr, cellSize) {
+const myJson32 = [];
+const myJson = [];
+
+function fillCanvas(arr, cellSize) {
+    let k = 0;
     for (let i = 0; i < cellSize; i++) {
-    for(let j =0; j < cellSize; j++) {
-        ctx.fillStyle = arr[i][j];
-        ctx.fillRect(i*(512/cellSize), j*(512/cellSize), (512/cellSize), (512/cellSize));
-    }
-  }}
-
- fetch('./data/4x4.json')
-  .then(response => response.json())
-  .then(body => body.forEach(element => element.forEach(item => myJson.push('#'+item))));
-
- fetch('./data/4x4.json')
-  .then(response => response.json())
-  .then(body => body.forEach((element,indexI) => {element.forEach(
-        (item,indexJ) => {
-            ctx.fillStyle = '#'+item;
-            ctx.fillRect(indexI*(512/4), indexJ*(512/4), (512/4), (512/4));
+        for (let j = 0; j < cellSize; j++) {
+            ctx.fillStyle = arr[k];
+            ctx.fillRect(i * (512 / cellSize), j * (512 / cellSize), (512 / cellSize), (512 / cellSize));
+            k++;
         }
-      )}
-  
-  ));
+    }
+}
+
+fetch('./data/4x4.json')
+    .then((response) => response.json())
+    .then((body) => body.forEach((element) => element.forEach((item) => myJson.push(`#${item}`))));
+
+fetch('./data/4x4.json')
+    .then((response) => response.json())
+    .then((body) => body.forEach((element, indexI) => {
+        element.forEach(
+            (item, indexJ) => {
+                ctx.fillStyle = `#${item}`;
+                ctx.fillRect(indexI * (512 / 4), indexJ * (512 / 4), (512 / 4), (512 / 4));
+            },
+        );
+    }));
 
 fetch('./data/32x32.json')
-  .then(response => response.json())
-  .then(body => body.forEach(element => element.forEach(item => myJson32.push(item))));
+    .then((response) => response.json())
+    .then((body) => body.forEach((element) => element.forEach((item) => myJson32.push(item))));
 
- 
-  
-  document.querySelectorAll('.button').forEach( btn => btn.addEventListener('click',function(){
-      if (btn.textContent === 'image') {
+
+document.querySelectorAll('.button').forEach((btn) => btn.addEventListener('click', () => {
+    if (btn.textContent === 'image') {
         picture.src = './data/image.png';
-        picture.onload = function() { 
-            ctx.drawImage(picture, 0, 0, 512, 512); 
-        }
-      } else if (btn.textContent === '4x4'){
-            ctx.clearRect(0,0,512,512);
-            cellSize = 4;
-            fillCanvas(myJson, cellSize);
+        picture.onload = function () {
+            ctx.drawImage(picture, 0, 0, 512, 512);
+        };
+    } else if (btn.textContent === '4x4') {
+        ctx.clearRect(0, 0, 512, 512);
+        cellSize = 4;
+        fillCanvas(myJson, cellSize);
 
-      }
-      else {
-      ctx.clearRect(0,0,512,512);
-      cellSize = 32;
-      let k =0;
+    } else {
+        ctx.clearRect(0, 0, 512, 512);
+        cellSize = 32;
+        let k = 0;
         for (let i = 0; i < cellSize; i++) {
-        for(let j =0; j < cellSize; j++) {
-            ctx.fillStyle = `rgb(${myJson32[k][0]},${myJson32[k][1]},${myJson32[k][2]})`;
-            ctx.fillRect(i*(512/cellSize), j*(512/cellSize), (512/cellSize), (512/cellSize));
-            k++;
+            for (let j = 0; j < cellSize; j++) {
+                ctx.fillStyle = `rgb(${myJson32[k][0]},${myJson32[k][1]},${myJson32[k][2]})`;
+                ctx.fillRect(i * (512 / cellSize), j * (512 / cellSize), (512 / cellSize), (512 / cellSize));
+                k++;
             }
         }
-      
+
     }
-  }))
- 
-  function fillCanvas(arr, cellSize) {
-      let k =0;
-    for (let i = 0; i < cellSize; i++) {
-    for(let j =0; j < cellSize; j++) {
-        ctx.fillStyle = arr[k];
-        ctx.fillRect(i*(512/cellSize), j*(512/cellSize), (512/cellSize), (512/cellSize));
-        k++;
-    }
-}}
-
-
-  
-
-
-
-
-
-/*for (let i = 0; i < myJson.length; i++) {
-    console.log(myJson[i]);
-}*/
-/*for (let i = 0; i < myJson.length; i++) {
-    for(let j =0; j < myJson.length; j++) {
-        console.log(myJson[i][j]);
-    }
-}*/
-/*fetch('./data/4x4.json')
-                .then(response => response.json())
-                .then(data => data.map(arr => { return arr.map(item =>{ return '#'+item})}))*/
+}));
